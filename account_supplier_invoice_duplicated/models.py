3079b0ec027fc80bbca6c5ee20e9de79b9388b73
David <david.martinez@dmpyme.com>
2015-06-28 16:43:03 +0200

    chequeo del proveedor

--- account_supplier_invoice_duplicated/models.py
+++ account_supplier_invoice_duplicated/models.py
@@ -7,11 +7,11 @@ class Duplicate_Invoice(models.Model):
     _inherit = 'account.invoice' 
     
     @api.multi
-    def check_invoice_duplicate(self, reference):
+    def check_invoice_duplicate(self, reference, supplier_id):
         if not reference:
             return
         msg = 'La referencia de factura ' + reference + ' ya ha sido creada!' 
-        numero = self.search_count([('type','=','in_invoice'),('reference','=',reference)])
+        numero = self.search_count([('type','=','in_invoice'),('reference','=',reference),('partner_id','=',supplier_id)])
         if numero > 0:
             raise Warning(msg)
         return 
